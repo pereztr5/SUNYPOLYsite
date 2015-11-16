9981efaf901ad531013f21ec48fc3969340c4584
Tony Perez <pereztr@fang.cs.sunyit.edu>
2015-11-16 00:44:19 -0500

    Finish cs490 hw7

--- cs490web/hw7/script.js
+++ cs490web/hw7/script.js
@@ -1,177 +1,136 @@
-// Create empty object array
-var classes = [
-    {
-        "studentName":"Bob",
-        "courseName":"cs490",
-        "imgFilename":"http://placehold.it/140x140"
-    },
-    {
-        "studentName":"Linda",
-        "courseName":"cs490",
-        "imgFilename":"http://placehold.it/140x140"
-    }
-]
-
 $(document).ready(function () {
-	preloadTable();
+    // click function for either Course or student button
+    $(".loadInfo").click(function() {
+        //Empty the info div
+        $("#info").empty();
+
+        // Make initial panel
+        var panel = $("<div/>", { "class": "panel panel-default"});
+        $("#info").append(panel);
+
+        // Load either the course or student
+        loadData($(this).data("type"));
+    });
+
+    // the panel does not exit until the info div is loaded
+    // We wait to detect the click for the font awesome in tag i
+    $("#info").on("click", "i", function() {
+        toggleThis(this);
+    });
+
 });
 
-// Used to put placeholders then empty object array
-function preloadTable() {
-    for (n of classes) {
-        if (n.courseName == "cs490") {
-        $("#cs490List").empty();
-        } else if (n.courseName == "ncs490") {
-        $("#ncs490List").empty();
-        }
-    }
+// Use one function to retrieve either the Student or Course data
+function loadData(objectType) {
 
-    makeList();
-}
-
-function getDataApi() {
-$.get("http://web.cs.sunyit.edu/~lampej/web/api/Student/getAll")
+// Javascript to get either the Student or Course information but
+// the first letter must be capitalized
+// Note: Loads all the data as soon as the Student or Course Button 
+// is clicked which is not effiecient
+$.get("http://web.cs.sunyit.edu/~lampej/web/api/"
+    + objectType.charAt(0).toUpperCase() + objectType.slice(1)
+    + "/getAll")
 	.done(function (data) {
-		$("#results").empty();
-		$("#results").append(JSON.stringify(data, null, '\t'));
+        jQuery.each(data, function(index, type) {
+            // Make the panel body
+            var panelBody = $( "<div/>", {"class": "panel-body"})
+            // Add content based on which button clicked
+            if (objectType == "student") {
+                loadStudentData(panelBody, type);
+            } else {
+                panelBody.append("<p>Max students: <strong>"
+                    + type.max_students + "</strong></p>");
+                loadStudentsCourse(panelBody, type.coursenum);
+            }
+            panelBody.hide();
+
+            // Make the panel heading
+            var panelHead = $( "<div/>", { 
+                "class": "panel-heading",
+                "data-id": objectType.id
+            });
+
+            // load font awesome icon
+            panelHead.append("<i class='fa fa-plus'></i> ");
+
+            // Load information Depending on which button clicked
+            panelHead.append( function() {
+                    if (objectType == "student") {
+                        return "<strong>" + type.name + "</strong>"
+                    } else {
+                        return "<strong>" + type.coursenum + ": " + type.name + "</strong>"
+                    }
+            });
+
+            // Make the whole panel and put everything together
+            //var panel = $("<div/>", { "class": "panel panel-default"});
+            $(".panel").append(panelHead);
+            $(".panel").append(panelBody);
+
+            // Load the panel
+            //$("#info").append(panel);
+            //$(".panel-heading").append(JSON.stringify(data, null, '\t'));
+        })
 	})
 	.fail(function (jqXHR, textStatus, errorThrown) {
-		console.log("Error calling Student/getAll: " + errorThrown);
+		console.log("Error calling Course/getAll: " + errorThrown);
 	});
 }
 
-// Fill the object array with new information
-function addClass(sName, cName) {
-    var newCourse = {};
+function toggleThis (toggler) {
+    $(toggler).parent(".panel-heading").next(".panel-body").slideToggle(300);
 
-    newCourse.studentName = sName;
-    newCourse.courseName = cName;
-    classes.push(newCourse);
-}
-
-// Disable Button and add the cog spinner
-function disableButton() {
-    $("#add").attr("disabled", "disabled"); 
-    $("#loading").append("<i class='fa fa-cog fa-spin' id='loadCog'></i>");
-}
-
-function loadTable() {
-    // Save the input so they cannot edit it while it is "loading"
-    var sName = $("#student").val();
-    var cName = $("#class").val();
-
-    if (sName.length != 0) {
-        // Remove error highlight class if there
-        $("#studentName").removeClass("has-error")
-        // disable the button first and add the spinner
-        disableButton();
-        // Set the time out for 3 sec
-        setTimeout(function () {
-            addClass(sName, cName);
-            
-            // Get rid of only a certain class list
-            for (n of classes) {
-                if (n.courseName == "cs490") {
-                $("#cs490List").empty();
-                } else if (n.courseName == "ncs490") {
-                $("#ncs490List").empty();
-                }
-            }
-
-            makeList();
-            // Renable button
-            $("#add").removeAttr("disabled");
-            // Remove the cog spinner
-            $("#loadCog").remove();
-        }, 3000);
+    if ($(toggler).hasClass("fa-minus")) {
+        $(toggler).removeClass("fa-minus");
+        $(toggler).addClass("fa-plus");
     } else {
-        $("#studentName").addClass("has-error");
+        $(toggler).removeClass("fa-plus");
+        $(toggler).addClass("fa-minus");
     }
 }
 
-function makeList() {
-    for (i=0; i < classes.length; i++) {
-        //var list = $("<button type='button' class='list-group-item' data-toggle='modal' data-target='#currentStudent'></button>");
-        // Need to add data-target='#myModal'
-
-        var list = $('<button/>').attr({
-            type: "button",
-            class: "list-group-item",
-            "data-toggle": "modal",
-            "data-target": "#currentStudent",
-            onclick: 'makeModal("' + classes[i].studentName + '",' + i + ')'
-        });
-        list.append(classes[i].studentName);
-
-        if (classes[i].courseName == "cs490") {
-            $("#cs490List").append(list);
-        } else if (classes[i].courseName == "ncs490") {
-            $("#ncs490List").append(list);
-        }
-    }
-}
-
-// Make the modal by taking in the name and index in the classes object array
-// For extra credit
-function makeModal(name, i) {
-    // Clear it out first
-    $(".hiddenModal").empty();
-
-    // Using bootstrap modal
-    // Made variables for each tag just to practice and keep tings organized
-    var fButton = $('<button/>').attr({
-        type: "button",
-        class: "btn btn-default",
-        "data-dismiss": "modal"
-    });
-
-    var footer = $('<div/>').attr({class: "modal-footer"}); 
-
+// Load the student data into the panel body
+function loadStudentData (target, student){
     var img = $('<img/>').attr({
-        src: classes[i].imgFilename,
-        alt: classes[i].studentName + " image",
+        alt: student.name + " image",
         class: "img-thumbnail"
-    });
+    });    
 
-    var body = $('<div/>').attr({class: "modal-body"}); 
+    // If there is a photo use it if not then use the placeholder
+    if (student.photo_filename.length > 0) {
+        img.attr({src: student.photo_filename});
+    } else {
+        img.attr({src: "placeholder.png"});
+    }
 
-    var title = $('<h4/>').attr({
-        class: "modal-title",
-        id: "myModalLabel",
-    });
-
-    var hButton = $("<button type='button' class='close' data-dismiss='modal' aria-label='Close'><span aria-hidden='true'>&times;</span></button>");
-
-    var header = $('<div/>').attr({class: "modal-header"}); 
-
-    var content = $('<div/>').attr({class: "modal-content"});
-
-    var dialog = $('<div/>').attr({
-        class: "modal-dialog",
-        role: "docuemnt"
-    });
-
-    var modal = $('<div/>').attr({
-        class: "modal fade",
-        id: "currentStudent",
-        tabindex: "-1",
-        role: "dialog",
-        "aria-labelledby": "myModalLabel"
-    });
-
-    // Append each tag into each other making sure they are in order as well
-    fButton.append("Close");
-    footer.append(fButton);
-    body.append(img);
-    title.append(name);
-    header.append(hButton);
-    header.append(title);
-    content.append(header);
-    content.append(body);
-    content.append(footer);
-    dialog.append(content);
-    modal.append(dialog);
-
-    $(".hiddenModal").append(modal);
+    var id = $('<p/>').html("ID: " + student.id);
+    var level = $('<p/>').html("Level: " + student.level);
+    var owner = $('<p/>').html("Owner: " + student.owner);
+    var active = $('<p/>').html("Active: " + function() {
+        if (student.active == 1) {
+            return "yes";
+        } else {
+            return "no";
+        }
+    }());
     
+    target.append(img);
+    target.append("<br><br>");
+    target.append(id);
+    target.append(level);
+    target.append(owner);
+    target.append(active);
+}
+
+// Load course information based on the courses
+function loadStudentsCourse (target, courseNum) {
+    target.append("<h5>Students Enrolled:</h5>");
+    // Get students in the course and load each on in the panel
+    $.get("http://web.cs.sunyit.edu/~lampej/web/api/Student/getStudentsInCourse/"
+        + courseNum)
+        .done(function (data) {
+            jQuery.each(data, function(index, student) {
+                target.append($('<p/>').html(student.name));
+            });
+        });
 }
